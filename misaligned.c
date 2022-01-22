53a02da5bd544c6d14c8ba10cf87bc8803c3f717
SudeshnaMukherjee94 <91111960+SudeshnaMukherjee94@users.noreply.github.com>
2022-01-22 18:57:36 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -8,21 +8,22 @@ int printColorMap() {
     const char* ColorPairsTable[25][3] = {}; //Defining a 2D array to store the 25 color pairs along with pair numbers
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
-            const char PairNumber = (i * 5 + j);
-            ColorPairsTable[j][i]= &PairNumber; //Storing the pair numbers for each pair
+            int PairNumber[25] = {};
+            //Storing the color table values from the buggy printf statement to test later
+            PairNumber[i+j] = (i * 5 + j);
             ColorPairsTable[j][i+1] = majorColor[i]; //Column 1 for storing major colors
             ColorPairsTable[j][i+2] = minorColor[i]; //Column 2 for storing minor colors
             printf("%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[i]);
-            printf("%s | %s | %s\n", ColorPairsTable[j][i], ColorPairsTable[j][i+1], ColorPairsTable[j][i+2]); //Printing the array to remove warnings
-        }
+            }
     }
+    printf(" %s | %s\n", ColorPairsTable[j][i+1], ColorPairsTable[j][i+2]); //Printing the first pair to remove warnings
     return i * j;
 }
 
 int main() {
     int result = printColorMap();
     assert(result == 25);
-    //assert(ColorPairsTable[1][1] == "1" & ColorPairsTable[1][2] == "White" & ColorPairsTable[1][3] == "Blue") //Checking if the first color pair in the table is White and Blue
+    //assert(ColorPairsTable[1][1] == "White" & ColorPairsTable[1][2] == "Blue") //Checking if the first color pair in the table is White and Blue
     printf("All is well (maybe!)\n");
     return 0;
 }
