ee1c37dec41ffd02f7b38c73b4551c248f587cec
SudeshnaMukherjee94 <91111960+SudeshnaMukherjee94@users.noreply.github.com>
2022-01-23 01:42:34 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -16,7 +16,7 @@ int printColorMap() {
             printf("%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[i]);
             }
     }
-    if (ColorPairsTable[1][1] == "White" & ColorPairsTable[1][2] == "Blue") //Checking if the first color pair in the table is White and Blue
+    if ((ColorPairsTable[1][1] == "White") & (ColorPairsTable[1][2] == "Blue")) //Checking if the first color pair in the table is White and Blue
         return 1;
     else
         return 0;
