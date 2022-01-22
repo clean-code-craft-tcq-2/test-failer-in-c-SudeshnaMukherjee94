e5ce3336c05b4887dee5796a76a022841f62d13e
SudeshnaMukherjee94 <91111960+SudeshnaMukherjee94@users.noreply.github.com>
2022-01-22 18:34:10 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -5,7 +5,7 @@ int printColorMap() {
     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
     int i = 0, j = 0;
-    const char* ColorPairsTable[25][3] = {}; //Defining a 2D array to store the 25 color pairs along with pair numbers
+    char* ColorPairsTable[25][3] = {}; //Defining a 2D array to store the 25 color pairs along with pair numbers
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
             ColorPairsTable[j][i]= (i * 5 + j); //Storing the pair numbers for each pair
