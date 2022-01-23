f78a08f32a6a29b6a211412f815ac1bf3d0df359
SudeshnaMukherjee94 <91111960+SudeshnaMukherjee94@users.noreply.github.com>
2022-01-23 15:15:09 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <assert.h>
+#include "networkAlert.c"
 
 int alertFailureCount = 0;
 
-int networkAlertStub(float celcius) {
+/*int networkAlertStub(float celcius) {
     printf("ALERT: Temperature is %.1f celcius.\n", celcius);
     // Return 200 for ok
     // Return 500 for not-ok
@@ -11,7 +12,7 @@ int networkAlertStub(float celcius) {
     // Let us consider the range for temperature to be "ok" as 35(=95F) Celcius to 37.5Celcius(=99.5F) 
     // -> Beyond this range the stub should sent 500 for "not-ok" -> but it does not do so due to faulty coding
     return 200;
-}
+}*/ 
 
 int alertInCelcius(float farenheit) {
     float celcius = (farenheit - 32) * 5 / 9;
