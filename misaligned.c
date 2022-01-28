fe248d4594d1e0565aa063098814bede8240fa20
SudeshnaMukherjee94 <91111960+SudeshnaMukherjee94@users.noreply.github.com>
2022-01-28 11:46:20 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -1,17 +1,26 @@
 #include <stdio.h>
 #include <assert.h>
 
-int printColorMap() {
+int FormColorMap()  {
     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
     int i = 0, j = 0;
     const char* ColorPairsTable[25][3] = {}; //Defining a 2D array to store the 25 color pairs 
+    
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
             //Storing the color table values from the buggy printf statement to test later
             ColorPairsTable[i*5+j][0] = majorColor[i]; //Column 1 for storing major colors
             ColorPairsTable[i*5+j][1] = minorColor[i]; //Column 2 for storing minor colors
-            printf("%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[i]);
+            }
+    }
+    return ColorPairsTable;
+}
+
+int printColorMap(const char* ColorPairsTable) {
+    for(i = 0; i < 5; i++) {
+        for(j = 0; j < 5; j++) {
+            printf("%d | %s | %s\n", i * 5 + j, ColorPairsTable[i*5+j][0], ColorPairsTable[i*5+j][1]);
             }
     }
     if (((ColorPairsTable)[0][0] == majorColor[0]) & (ColorPairsTable[0][1] == minorColor[0])) //Checking if the first color pair in the table is White and Blue
@@ -32,7 +41,8 @@ int printColorMap() {
 }
 
 int main() {
-    int result = printColorMap();
+    const char* ColorMap = FormColorMap();
+    int result = printColorMap(ColorMap);
     assert(result == 0);
     printf("All is well (maybe!)\n");
     return 0;
