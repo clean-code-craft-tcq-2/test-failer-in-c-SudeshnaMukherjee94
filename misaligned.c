3f615b6cb7a8a5000f6b1aa7123acb92a818956c
SudeshnaMukherjee94 <91111960+SudeshnaMukherjee94@users.noreply.github.com>
2022-02-02 23:36:39 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -21,7 +21,10 @@ int printColorMap() {
     int i = 0, j = 0;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
+            if ((i * 5 + j +1)<10)
             printf(" %d | %s | %s\n", i * 5 + j +1, ColorPairsTable[i][0], ColorPairsTable[i*5+j][1]);
+            else
+            printf("%d | %s | %s\n", i * 5 + j +1, ColorPairsTable[i][0], ColorPairsTable[i*5+j][1]);
             }
     }
             return i*j;
@@ -32,12 +35,7 @@ int main() {
     int result = printColorMap();
     assert(totalItems == 25);
     assert(result == 25);
-    //assert (((ColorPairsTable[0][0] == majorColor[0]) & (ColorPairsTable[0][1] == minorColor[0])) == 1); //Checking if the first color pair in the table is White and Blue
-    //assert (((ColorPairsTable[1][0] == majorColor[0]) & (ColorPairsTable[1][1] == minorColor[1])) == 1); //Checking if the second color pair in the table is White and Orange
-    //assert (((ColorPairsTable[2][0] == majorColor[0]) & (ColorPairsTable[2][1] == minorColor[2])) == 1); //Checking if the third color pair in the table is White and Green
-    //assert (((ColorPairsTable[3][0] == majorColor[0]) & (ColorPairsTable[3][1] == minorColor[3])) == 1); //Checking if the forth color pair in the table is White and Brown
-    //assert (((ColorPairsTable[4][0] == majorColor[0]) & (ColorPairsTable[4][1] == minorColor[4])) == 1); //Checking if the fifth color pair in the table is White and Slate
-    //assert (((ColorPairsTable[5][0] == majorColor[0]) & (ColorPairsTable[5][1] == minorColor[0])) == 1); //Checking if the sixth color pair in the table is Red and Blue
+    assert (((&ColorPairsTable[0][0] == &majorColor[0]) & (&ColorPairsTable[0][1] == &minorColor[0])) == 1); //Checking if the first color pair in the table is White and Blue
     printf("All is well (maybe!)\n");
     return 0;
 }
