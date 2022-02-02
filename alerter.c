d0053e7fcfcfe4ecf284cb8acb7825f05dd63da9
SudeshnaMukherjee94 <91111960+SudeshnaMukherjee94@users.noreply.github.com>
2022-02-03 02:14:21 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -6,16 +6,6 @@ int alertFailureCount = 0;
 
 // Shifted the stub part(the commented part below) to a seperated file so that it can be modified later during integration without disturbing the main production code
 
-/*int networkAlertStub(float celcius) {
-    printf("ALERT: Temperature is %.1f celcius.\n", celcius);
-    // Return 200 for ok
-    // Return 500 for not-ok
-    // stub always succeeds and returns 200
-    // Let us consider the range for temperature to be "ok" as 35(=95F) Celcius to 37.5Celcius(=99.5F) 
-    // -> Beyond this range the stub should sent 500 for "not-ok" -> but it does not do so due to faulty coding
-    return 200;
-}*/ 
-
 int alertInCelcius(float farenheit) {
     float celcius = (farenheit - 32) * 5 / 9;
     int returnCode = networkAlertStub(celcius);
@@ -26,12 +16,6 @@ int alertInCelcius(float farenheit) {
         // Add a test below to catch this bug. Alter the stub above, if needed.
         alertFailureCount += 0;
     }
-    //Test Case :- If input temperature range crosses the normal range of 35(=95F) Celcius to 37.5Celcius(=99.5F) 
-    //             AND still alert failure counter is not greater than 0 --> Test case failed
-    if (((farenheit<95) | (farenheit>99.5)) & !(alertFailureCount > 0))
-        return 1; //Test failed
-    else
-        return 0; //Test passed
 }
 
 int main() {
