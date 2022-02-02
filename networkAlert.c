e07449e12197ece6f39f48116bebec691a9d6d9d
SudeshnaMukherjee94 <91111960+SudeshnaMukherjee94@users.noreply.github.com>
2022-02-03 02:15:57 +0530

    Update networkAlert.c

--- networkAlert.c
+++ networkAlert.c
@@ -4,7 +4,7 @@
 // This file is for switching between the test env to integration env
 // An OR Logic can be added here to switch between the Stub code and the code for sending the ALERT over network
 
-int networkAlertStub(float celcius) {
+int networkAlertStubOk(float celcius) {
     printf("ALERT: Temperature is %.1f celcius.\n", celcius);
     // Return 200 for ok
     // Return 500 for not-ok
@@ -12,4 +12,15 @@ int networkAlertStub(float celcius) {
     // Let us consider the range for temperature to be "ok" as 35(=95F) Celcius to 37.5Celcius(=99.5F) 
     // -> Beyond this range the stub should sent 500 for "not-ok" -> but it does not do so due to faulty coding
     return 200;
+}
+
+
+int networkAlertStubNotOk(float celcius) {
+    printf("ALERT: Temperature is %.1f celcius.\n", celcius);
+    // Return 200 for ok
+    // Return 500 for not-ok
+    // stub always succeeds and returns 200
+    // Let us consider the range for temperature to be "ok" as 35(=95F) Celcius to 37.5Celcius(=99.5F) 
+    // -> Beyond this range the stub should sent 500 for "not-ok" -> but it does not do so due to faulty coding
+    return 500;
 }
