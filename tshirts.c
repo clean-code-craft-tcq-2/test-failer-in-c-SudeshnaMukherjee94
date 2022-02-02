87d299bb48ca79bcbe4d2b5223fe049112fe0834
SudeshnaMukherjee94 <91111960+SudeshnaMukherjee94@users.noreply.github.com>
2022-02-02 10:22:38 +0530

    Update tshirts.c

--- tshirts.c
+++ tshirts.c
@@ -3,13 +3,14 @@
 
 char size(int cms) {
     char sizeName = '\0';
-    if(cms < 38) {
+    if(cms > 36 && cms < 38) {
         sizeName = 'S';
     } else if(cms > 38 && cms < 42) {
         sizeName = 'M';
-    } else if(cms > 42) {
+    } else if(cms > 44 && cms > 42) {
         sizeName = 'L';
     }
+    else sizeName = '\0';
     return sizeName;
 }
 
