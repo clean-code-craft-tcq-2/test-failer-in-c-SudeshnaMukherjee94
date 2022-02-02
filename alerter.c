8faee65ee1eaea676257f3e569d0c489adb037d9
SudeshnaMukherjee94 <91111960+SudeshnaMukherjee94@users.noreply.github.com>
2022-02-03 02:24:23 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -6,7 +6,7 @@ int alertFailureCount = 0;
 
 // Shifted the stub part(the commented part below) to a seperated file so that it can be modified later during integration without disturbing the main production code
 
-int alertInCelcius(float farenheit) {
+int alertInCelcius(float farenheit, int NetworkResponse) {
     float celcius = (farenheit - 32) * 5 / 9;
     int returnCode = networkAlertStub(celcius);
     if (returnCode != 200) {
@@ -19,10 +19,8 @@ int alertInCelcius(float farenheit) {
 }
 
 int main() {
-    assert (alertInCelcius(99)==0); // Test for "ok" value
-    assert (alertInCelcius(96)==0); // Test for "ok" value
-    assert (alertInCelcius(303.6)==0); // Test for "not-ok" value
-    assert (alertInCelcius(90)==0); // Test for "not-ok" value
+    assert (alertInCelcius(303.6,networkAlertStubOk(303.6)) == 0); // Test for "ok" value
+    assert (alertInCelcius(400.5,networkAlertStubNotOk(400.5)) > 0); // Test for "not-ok" value
     printf("%d alerts failed.\n", alertFailureCount);
     printf("All is well (maybe!)\n");
     return 0;
